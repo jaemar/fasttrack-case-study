ab366779d8e19a5656bd95a44ff63b48da47a51d
bry <bry@bryanbibat.net>
2011-03-23 14:37:01 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3'
+gem 'devise'
+gem 'paperclip'
+gem 'jquery-rails'
+gem 'will_paginate', '3.0.pre2'
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FasttrackCaseStudy::Application.config.secret_token = 'adf77db47fa81c8069b10cfe910899668dcafe4840766713a14f58071067bbc977044667ca5665582eb1644ef2a0f31afb58e5dc5e03d211a4425f890bf32096'
