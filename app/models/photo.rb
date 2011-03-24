26cf1ce6f2c25e4691cda622bffaf43f1abf8884
Alfonso Juan Dillera <aj_dillera@yahoo.com>
2011-03-24 15:46:48 +0800

    Valildation that a photo has been uploaded has been added.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
+
   has_attached_file :photo, :styles => { :medium => "100x100>", :thumb => "50x50>" }
   validates_attachment_size :photo, :less_than => 2.megabytes
+  validates_attachment_presence :photo
 end
