dcfaee02fbb8d728c0a9204a7868318a987c0911
Alfonso Juan Dillera <aj_dillera@yahoo.com>
2011-03-24 15:50:05 +0800

    Added conditions for photo uploading.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,5 @@ class Photo < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "100x100>", :thumb => "50x50>" }
   validates_attachment_size :photo, :less_than => 2.megabytes
   validates_attachment_presence :photo
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 end
