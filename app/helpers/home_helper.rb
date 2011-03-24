6d61f04c11712ea299d5a7306ea664757fbcd01c
Jaemar Joseph Ramos <jaemar.ramos@gmail.com>
2011-03-24 15:27:35 +0800

    add owned albums and shared albums in home index

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def latest_photo(album_id)
     Photo.find(:last, :conditions => ['album_id = ?', album_id])
   end
+
+  def get_album_name(album_id)
+    Album.find_by_id(album_id)
+  end
 end
