e08476d98db8005d4f586ba6ba9e6a27cadf8dc2
Jaemar Joseph Ramos <jaemar.ramos@gmail.com>
2011-03-24 12:04:34 +0800

    change the routes for deleting albums, show only the enabled albums

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ FasttrackCaseStudy::Application.routes.draw do
   devise_for :users
 
   match "albums/:id/enable" => "albums#enable", :via => :get, :as => 'enable'
-  match "albums/:id/disable" => "albums#disable", :via => :get, :as => 'disable'
+  match "albums/:id/delete" => "albums#delete", :via => :get, :as => 'delete'
 
   match 'photo/:id' => 'photos#move', :via => :put, :as => 'update_move_photo'
   match 'photo/:id/move' => 'photos#index', :via => :get, :as => 'move_photo'
