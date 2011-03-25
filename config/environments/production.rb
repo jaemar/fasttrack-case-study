e4e5cdf4eb979fa5a741e227713b0449a561f99f
bry <bry@bryanbibat.net>
2011-03-25 10:16:02 +0800

    Add Capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ FasttrackCaseStudy::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'mockups.bryanbibat.net/fasttrack' }
 end
