0222ac6a8b02d7d7c984123678fa840c3ad7d1ea
Sarrah Gasendo <sarrahgasendo@gmail.com>
2011-03-25 12:49:26 +0800

    modified status filter for invitation feature in search

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,4 +6,8 @@ module HomeHelper
   def get_album_name(album_id)
     Album.find_by_id(album_id)
   end
+
+  def check_status(user_id, target_id)
+    Invitation.find(:first, :conditions => ['user_id = ? AND invited_user_id = ?', user_id, target_id])
+  end
 end
