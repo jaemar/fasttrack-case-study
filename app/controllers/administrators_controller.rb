975d0520f3deb4f6db4ac9262cee1a41b982542b
Alfonso Juan Dillera <aj_dillera@yahoo.com>
2011-03-25 14:56:41 +0800

    Admin page showing blocked users

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -4,6 +4,6 @@ class AdministratorsController < ApplicationController
     @albums = Album.all
     @registered_users_today = User.where("created_at BETWEEN ? AND ?", Time.now.change(:hour => 0, :min => 0, :sec => 1), Time.now.change(:hour => 23, :min => 59, :sec => 59))
     @uploaded_photos_today = Photo.where("created_at BETWEEN ? AND ?", Time.now.change(:hour => 0, :min => 0, :sec => 1), Time.now.change(:hour => 23, :min => 59, :sec => 59))
-    @blocked_users = Invitation.order("updated_at DESC").find_all_by_status(Invitation::BlockStatus)
+    @blocked_users = Invitation.group("user_id, updated_at").having(["status = ?", Invitation::BlockStatus.to_i]).order('updated_at DESC')
   end
 end
--- app/helpers/administrators_helper.rb
+++ app/helpers/administrators_helper.rb
@@ -1,6 +1,9 @@
 module AdministratorsHelper
   def change_datetime_to_date(datetime)
     date = datetime.strftime( "%d / %m / %Y" )
-    return date
+  end
+
+  def get_block_count(user_id, updated_at)
+    block_count = Invitation.where("(user_id = ?) AND (updated_at BETWEEN ? AND ?)", user_id,Time.now.change(:year => updated_at.strftime("%Y").to_i, :month => updated_at.strftime("%m").to_i, :day => updated_at.strftime("%d").to_i, :hour => 0, :min => 0, :sec => 1), Time.now.change(:year => updated_at.strftime("%Y").to_i, :month => updated_at.strftime("%m").to_i, :day => updated_at.strftime("%d").to_i, :hour => 23, :min => 59, :sec => 59)).count
   end
 end
